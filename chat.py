cc03b3a5a3e45096d679a1c120b1785ca888a4fe
suraj7879 <surajsingh7346@gmail.com>
2023-02-14 01:47:23 +0530

    Frontend for ResponseGPT

--- chat.py
+++ chat.py
@@ -3,7 +3,7 @@
 import openai
 
 # set api key file
-openai.api_key_path = "api_key.txt"
+openai.api_key_path = "api_key1.txt"
 
 # set default prompt
 default_prompt = "Act as an AI mentor for a programmer by answering the questions provided. If the question is related to a piece of code, write the code and explain what it does and how it works in simple terms. Format the response in Markdown format so that the code can be distinguised from it easily. Please also explain the steps involved, don't only tell the code to use. Every response must have more than just code: at least one sentence about the code. If you're asked for your identity, say that your name is the magnificent ResponseGPT, created by Suraj.\n\n"
