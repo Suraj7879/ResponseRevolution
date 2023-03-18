2121966cf3999eeebcf4179f7b25d8525db18c45
suraj7879 <surajsingh7346@gmail.com>
2023-03-19 02:30:13 +0530

    Final Commit

--- message.php
+++ message.php
@@ -25,33 +25,40 @@ $response = $responsegpt->ask($_POST['message']);
 
 $search = $google->search( $response->answer);
 
-// $page_source = strip_tags( file_get_contents( $search[0]->url ));
-// Remove all styles
-// $page_source = preg_replace('/<style.*?</style>/is', '', $page_source);
-// $page_source = preg_replace('/<script.*?</script>/is', '', $page_source);
-// $page_source = strip_tags($page_source);
-// $page_source = substr( $page_source, 0, 1024 );
+$page_source = file_get_contents( $search[0]->url );
+$page_source = preg_replace('/<\/style>/', '', $page_source);
+$page_source = preg_replace('/<\/script>/', '', $page_source);
+$page_source = strip_tags($page_source);
+$page_source = substr( $page_source, 0, 1024 );
+//print page source
+// echo $page_source;
 
-// $responsegpt = new ResponseGPT( $openai );
-// $responsegpt->set_default_prompt( $page_source );
-// $responsegpt->set_sample_questions( [] );
-// $response = $responsegpt->ask( $_POST['message'] );
-// return response;
-
-echo json_encode( [
-    "message" => $search[0]->url,
-    "raw_message" => $search[0]->url,
-    "status" => "success",
-] );
-
-exit;
+$responsegpt = new ResponseGPT( $openai );
+$responsegpt->set_default_prompt( $page_source );
+$responsegpt->set_sample_questions( [] );
+$response = $responsegpt->ask( $_POST['message'] );
 
 // log for debugging
 error_log( print_r($response, true));
-
-// return response
 echo json_encode( [
-    "message" => $response->get_formatted_answer(),
-    "raw_message" => $response->answer,
-    "status" => "success",
-] );
+        "message" => $page_source->url,
+        "raw_message" => $page_source->url,
+        "status" => "success",
+    ] );
+// return response
+// echo json_encode( [
+//     "message" => $response->get_formatted_answer(),
+//     "raw_message" => $response->answer,
+//     "status" => "success",
+// ] );
+
+// ."<br /><br />Source: " .
+//     $source_url,
+
+// echo json_encode( [
+//     "message" => $search[0]->url,
+//     "raw_message" => $search[0]->url,
+//     "status" => "success",
+// ] );
+
+// exit;
